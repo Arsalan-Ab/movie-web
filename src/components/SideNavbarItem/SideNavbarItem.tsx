de3fafd9fee492c12a9f7bb0002c10faee9658f9
arsalan <arsab7879@gmail.com>
2023-04-28 15:35:09 +0330

    create navbar

---
+++ src/components/SideNavbarItem/SideNavbarItem.tsx
@@ -0,0 +1,11 @@
+export interface SideNavbarItemProps {
+    name: string;
+    link: string;
+    icon: string;
+}
+
+const SideNavbarItem = (props: SideNavbarItemProps) => {
+    return null;
+};
+
+export default SideNavbarItem;
